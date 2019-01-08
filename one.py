b04840cf345f4d602488eb7f415b09ad8bdbb020
unknown <Bidur.Khanal@outlook.com>
2019-01-08 15:47:31 +0545

    add data

--- one.py
+++
@@ -1,5 +0,0 @@
-import memcache
-
-while True:
-    shared = memcache.Client(['127.0.0.1:11211'], debug=0)
-    shared.set('Value', 'Hello')
--- two.py
+++
@@ -1,8 +0,0 @@
-import memcache
-
-
-
-while True:
-    
-    shared = memcache.Client(['127.0.0.1:11211'], debug=0)
-    print shared.get('Value')
